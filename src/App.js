8b3c7ebb8d10e0e8ae68c389151e9c47f2c860cf
christian <christian.parks98@gmail.com>
2025-01-28 19:54:25 -0800

    first REAL commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ProgressBar from './components/progress-bar.component.js';
 import React, { useState, useEffect } from 'react';
 
 function App() {
@@ -11,11 +12,21 @@ function App() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentPhraseIndex((prevIndex) => (prevIndex + 1) % phrases.length);
-    }, 4400);
-
+    }, 4500);
     return () => clearInterval(interval);
   }, [phrases.length]);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const content = document.getElementById('destructibleHello');
+      if (content) {
+        content.remove();
+      }
+    }, 9000);
+
+
+    return () => clearTimeout(timeout);
+  }, []);
 
   return (
     <div className="App">
@@ -28,7 +39,9 @@ function App() {
       </div>
 
       <div className="content">
-        <p className="hello">{phrases[currentPhraseIndex]}</p>
+        <div>
+        </div>
+        <p className="hello" id='destructibleHello'>{phrases[currentPhraseIndex]}</p>
       </div>
     </div >
   );
